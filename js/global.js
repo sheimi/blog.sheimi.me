fc816ffed3aabb068ae83055e31c27b14ab48bc3
sheimi <sheimi.zhang@gmail.com>
2012-03-19 21:16:07 +0800

    a

---
+++ js/global.js
@@ -0,0 +1,18 @@
+//Google Plus One
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+//Google Plus One End 
+//Google Analytics 
+var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-29300274-1']);
+      _gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+//Google Analytics End
