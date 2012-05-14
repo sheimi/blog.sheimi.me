a3c80430efe29705a7319ca951314d0d86404f9b
sheimi <sheimi.zhang@gmail.com>
2012-05-14 09:08:28 +0800

    post

--- js/global.js
+++ js/global.js
@@ -1,17 +1,3 @@
-//Google Analytics 
-!function() {
-  var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-29300274-1']);
-        _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-}()
-//Google Analytics End
-
 //for footer
 /*
  * * author @lepture from june
