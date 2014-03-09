0a58f00adc631a1c265ee6ede71921080effa131
sheimi <sheimi.zhang@gmail.com>
2014-03-09 00:34:03 -0800

    update style

--- static/js/global.js
+++ static/js/global.js
@@ -5,11 +5,6 @@
     $('img[alt="content image"]').each(function(index, value) {
       return $(this).addClass('img-polaroid');
     });
-    $(document).on('click', '.header', function(e) {
-      return $('html, body').animate({
-        scrollTop: 0
-      });
-    });
     $(window).on('resize', function(e) {
       return windowResize();
     });
